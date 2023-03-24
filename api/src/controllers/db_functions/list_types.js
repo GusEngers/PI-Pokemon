fbca8fef3cadcece73699d928848c8c0edf63a22
GusEngers <gustavoengers6441@gmail.com>
2023-03-24 17:46:31 -0300

    Update listTypes function in db_functions
     - Updates the listTypes function in db_functions by replacing the existing listApiTypes call with a new call to listTypes.

---
+++ api/src/controllers/db_functions/list_types.js
@@ -0,0 +1,32 @@
+const { Type } = require('../../db');
+const { listApiTypes } = require('../api_functions/list_types');
+
+/**
+ * Crea los distintos tipos de pokemon
+ * @param types Lista con los tipos de pokemons traídos de la api
+ */
+async function createTypes(types) {
+  for (let i = 0; i < types.length; i++) {
+    await Type.create({ name: types[i].name });
+  }
+}
+
+/**
+ * Obtener los tipos de pokemons alojados en la base de datos.
+ * Si no hay nada alojado, se utiliza la api y luego se cargan esos valores a la base de datos.
+ * @returns Una lista con los tipos de pokemons
+ */
+async function listTypes() {
+  let types = await Type.findAll({});
+  if (!types.length) {
+    let typesApi = await listApiTypes();
+    await createTypes(typesApi);
+    types = await Type.findAll({});
+    return types;
+  }
+  return types;
+}
+
+module.exports = {
+  listTypes,
+};
--- api/src/routes/type.js
+++ api/src/routes/type.js
@@ -1,12 +1,13 @@
 const { Router } = require('express');
 const { listApiTypes } = require('../controllers/api_functions/list_types');
+const { listTypes } = require('../controllers/db_functions/list_types');
 
 const router = Router();
 
 // GET - Lista de tipos de pokemons
 router.get('/', async (req, res) => {
   try {
-    let data = await listApiTypes();
+    let data = await listTypes();
     res.json({ data });
   } catch (error) {
     res.status(404).json({ error: error.message });
